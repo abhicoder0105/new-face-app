73bd8bb358c5b46ad58ef738a02528f3b8301da6
abhicoder0105 <sharmaabhishek9770@gmail.com>
2022-01-09 16:11:21 +0530

    Added Reset Password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,14 @@ class UserMailer < ApplicationMailer
   default from: "sharmaabhishek9770@gmail.com"
 
   def welcome_email(email)
-    debugger
+    
     # @user = params[:sign_up]
     
     # @url  = 'http://example.com/login'
     mail(to: email, subject: 'Welcome to My Awesome Site')
   end
+
+  def confirm_user_email(email)
+    mail(to: email, subject: 'Welcome to My Awesome Site')
+  end  
 end

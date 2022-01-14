214296dac884556b60582f6fba4144c3a418e7c1
abhicoder0105 <sharmaabhishek9770@gmail.com>
2022-01-14 23:30:40 +0530

    Implemented edit  and delete on blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,6 +22,28 @@ class BlogsController < ApplicationController
     end
   end
 
+
+  def edit
+  	@blog = Blog.find(params[:id]);
+  end
+
+  def update
+  	 @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
     private
 
     def blog_params
